8fe94f92d99b2188dd8d55b063c8e9cdd792bfa2
anthev <cameronanthev@gmail.com>
2025-11-14 19:51:14 +1000

    Connect homepage to live plan API and refresh sliders

--- app/Http/Controllers/Api/PublicPlanController.php
+++ app/Http/Controllers/Api/PublicPlanController.php
@@ -39,7 +39,10 @@ class PublicPlanController extends Controller
                 })->values();
         });
 
-        return response()->json($plans);
+        return response()->json($plans)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
 
